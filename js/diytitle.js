f4d20d2c194cbd39520171bf0755bfe1a978696e
ShineShenCn <52003087+ShineShenCn@users.noreply.github.com>
2021-07-27 10:46:58 +0800

    更新到最新版本381

---
+++ js/diytitle.js
@@ -0,0 +1,16 @@
+//动态标题
+
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        document.title = 'w(ﾟДﾟ)w 不要走！再看看嘛！';
+        clearTimeout(titleTime);
+    }
+    else {
+        document.title = '♪(^∇^*)欢迎回来！' + OriginTitile;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
